6d789b5652750dd4d26e868075685c9d7571f5cd
Steve <srherzog@gmail.com>
2020-06-16 12:36:20 -0500

    Updated mk_config

--- py2http/service.py
+++ py2http/service.py
@@ -1,6 +1,7 @@
 from aiohttp import web
 import inspect
 import json
+from typing import Callable
 from warnings import warn
 
 from py2http.default_configs import default_configs
@@ -13,31 +14,51 @@ def method_not_found(method_name):
                            content_type='application/json')
 
 
-def mk_config(key, func, funcname, configs, defaults, **options):
+def mk_config(key, func, configs, defaults, **options):
+    """
+    Get a config value for a function. First checks the properties of the function,
+    then the configs, then the defaults.
+
+    :param key: The key to search for
+    :param func: The function associated with the config
+    :param configs: A config dict to search
+    :param defaults: The default configs to fall back on
+    :param **options: Additional options
+
+    :Keyword Arguments:
+        * *funcname*
+          The name of the function, if not the same as func.__name__
+        * *type*
+          The expected type of the output (use Callable for functions)
+    """
+    funcname = options.get('funcname', getattr(func, '__name__', None))
     result = getattr(func, key, configs.get(key, None))
     if isinstance(result, dict):
-        result = result.get(funcname, None)
+        dict_value = result.get(funcname, None)
+        if dict_value:
+            result = dict_value
+        elif options.get('type', None) is not dict:
+            result = None
     if result:
-        if options.get('is_func', None) and not callable(result):
-            warn(f'Config {key} is not callable, using default.')
-            result = defaults.get(key, None)
-        else:
-            expected_type = options.get('type', None)
-            if expected_type and not isinstance(result, expected_type):
-                warn(f'Config {key} does not match type {expected_type}, using default.')
-                result = defaults.get(key, None)
+        expected_type = options.get('type', None)
+        if not expected_type:
+            default_value = defaults.get(key, None)
+            assert default_value is not None, f'Missing default value for key {key}'
+            if callable(default_value):
+                expected_type = Callable
+            else:
+                expected_type = type(default_value)
+        assert isinstance(result, expected_type), f'Config {key} does not match type {expected_type}.'
     else:
         result = defaults.get(key, None)
-    assert callable(result), f'Invalid input mapper for function {key}, must be callable'
     return result
 
 
 def mk_route(function, **configs):
-    method_name = function.__name__
     # TODO: perhaps collections.abc.Mapping instead of dict?
-    input_mapper = mk_config('input_mapper', function, method_name, configs, default_configs)
-    input_validator = mk_config('input_validator', function, method_name, configs, default_configs)
-    output_mapper = mk_config('output_mapper', function, method_name, configs, default_configs)
+    input_mapper = mk_config('input_mapper', function, configs, default_configs)
+    input_validator = mk_config('input_validator', function, configs, default_configs)
+    output_mapper = mk_config('output_mapper', function, configs, default_configs)
     request_schema = getattr(input_mapper, 'request_schema', mk_input_schema_from_func(function))
     response_schema = getattr(output_mapper, 'response_schema', {})
 
@@ -72,12 +93,13 @@ def mk_route(function, **configs):
         print(f'final result: {type(final_result)}')
         return final_result
 
-    http_method = mk_config('http_method', function, method_name, configs, default_configs).lower()
+    http_method = mk_config('http_method', function, configs, default_configs).lower()
     if http_method not in ['get', 'put', 'post', 'delete']:
         http_method = 'post'
     web_mk_route = getattr(web, http_method)
-    path = mk_config('route', function, method_name, configs, default_configs)
+    path = mk_config('route', function, configs, default_configs)
     if not path:
+        method_name = function.__name__
         path = f'/{method_name}'
     route = web_mk_route(path, handle_request)
     openapi_path = mk_openapi_path(path, http_method, request_dict=request_spec, response_dict=response_spec)
@@ -90,12 +112,12 @@ def handle_ping():
 
 def run_http_service(functions, **configs):
     app = mk_http_service(functions, **configs)
-    port = mk_config('port', None, None, configs, default_configs)
+    port = mk_config('port', None, configs, default_configs)
     web.run_app(app, port=port)
 
 
 def mk_http_service(functions, **configs):
-    middleware = mk_config('middleware', None, None, configs, default_configs)
+    middleware = mk_config('middleware', None, configs, default_configs)
     app = web.Application(middlewares=middleware)
     routes = []
     openapi_config = configs.get('openapi', {})
@@ -118,5 +140,5 @@ def run_many_services(apps, **configs):
     app = web.Application()
     for route, subapp in apps.items():
         app.add_subapp(route, subapp)
-    port = mk_config('port', None, None, configs, default_configs)
+    port = mk_config('port', None, configs, default_configs)
     web.run_app(app, port=port)
