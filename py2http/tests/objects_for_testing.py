f75873a673177c44d4579717160ce51e98733462
twhalen <thor@otosense.com>
2020-07-14 09:46:00 -0700

    sum_of_args: a test func with *args and **kwargs

--- py2http/tests/objects_for_testing.py
+++ py2http/tests/objects_for_testing.py
@@ -46,3 +46,14 @@ def mult(x: float, y=1):
 # This one has every of the 4 combinations of (default y/n, annotated y/n)
 def formula1(w, /, x: float, y=1, *, z: int = 1):
     return ((w + x) * y) ** z
+
+
+# Only *args and **kwargs. Also has a doc with a doc test
+def sum_of_args(*args, **kwargs):
+    """Sums the args, and returns a copy of kwargs with the sum of args added to each value.
+
+    >>> sum_of_args(1,2,3, a=0, b=10)
+    {'a': 6, 'b': 16}
+    """
+    t = sum(args)
+    return {k: t + v for k, v in kwargs.items()}
