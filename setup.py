4386873645d6cf597335a8975343decdde9a20e9
Steve Herzog <steve.herzog@analog.com>
2020-10-29 21:19:25 -0500

    Publish v0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 # uncomment this for manual build
 setup_configs = dict(
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     include_package_data=True,
     platforms='any'
