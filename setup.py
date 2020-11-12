cd78c6f8ff5be9c449090012c3a28289187650eb
Steve Herzog <steve.herzog@analog.com>
2020-11-12 13:56:42 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 # uncomment this for manual build
 setup_configs = dict(
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(),
     include_package_data=True,
     platforms='any'
