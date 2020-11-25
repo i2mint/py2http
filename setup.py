c99a6cc2bc5104b7e8a35ec0d4110821a097ce57
GitHub CI Runner <vferon@pentalog.com>
2020-11-25 00:03:18 +0000

    **CI** Formatted code & Updated version number. [skip ci]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 # uncomment this for manual build
 setup_configs = dict(
-    version='0.1.16',
+    version='0.1.17',
     packages=find_packages(),
     include_package_data=True,
     platforms='any',
